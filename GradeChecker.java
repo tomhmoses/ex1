624306fa0acad21758d3a5e508036bb78a3dc9c2
Tom Moses <tom@tmoses.co.uk>
2018-10-28 12:57:27 +0000

    initial files

---
+++ GradeChecker.java
@@ -0,0 +1,40 @@
+package com.bham.pij.exercises.e1a;
+
+//TOM MOSES 1911437
+
+import java.util.Scanner;
+public class GradeChecker {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter an integer");
+		int grade = in.nextInt();
+		
+		if(GradeChecker.isValid(grade)) {
+			if (GradeChecker.isPass(grade)) {
+				System.out.println("Pass");
+			}
+			else {
+				System.out.println("Fail");
+			}
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+		
+
+	}
+	
+	
+	public static boolean isValid(int grade) {
+		boolean valid = 0 <= grade && grade <= 100;
+		return valid;
+	}
+	
+	public static boolean isPass(int grade) {
+		boolean pass = grade >= 40;
+		return pass;
+		
+	}
+
+}
---
+++ IDCreator.java
@@ -0,0 +1,40 @@
+package com.bham.pij.exercises.e1d;
+
+//TOM MOSES 1911437
+
+import java.util.Scanner;
+public class IDCreator {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter your full name:");
+		String name = in.nextLine();
+		System.out.println(IDCreator.createID(name));
+	}
+	
+	public static String createID(String input) {
+		if(input == null || input == "") {
+			return null;
+		}
+		String[] names = input.split("\\s+");
+		// "\\s+" is the regex whitespace
+		if(names.length > 3 || names.length < 2) {
+			return null;
+		}
+		
+		String ID = "";
+		ID += Character.toString(names[0].charAt(0));
+		if(names.length == 2) {
+			ID += "X";
+			ID += Character.toString(names[1].charAt(0));
+		}
+		else {
+			ID += Character.toString(names[1].charAt(0));
+			ID += Character.toString(names[2].charAt(0));
+		}
+		
+		
+		return ID.toUpperCase();
+	}
+
+}
